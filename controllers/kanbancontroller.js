991ba0bc0ce86ae7d227706eb71a933a17a35d49
Sachiiiiin <sachinpantha69@gmail.com>
2022-11-12 19:09:24 +0545

    Updated kanbancontroller

--- controllers/kanbancontroller.js
+++ controllers/kanbancontroller.js
@@ -7,7 +7,7 @@ const getAllTasks = async (req, res, next) => {
         await Task.find()
             .then(response => {
                 res.status(200).json({
-                    status: 'success',
+                    status: 'Success',
                     data: {
                         response
                     }
@@ -15,7 +15,7 @@ const getAllTasks = async (req, res, next) => {
             })
             .catch((err) => {
                 res.status(404).json({
-                    status: 'fail',
+                    status: 'Failed',
                     data: {
                         message: 'No any tasks found',
                         errorMessage: `${err}`
@@ -42,24 +42,34 @@ const addTask = async (req, res, next) => {
             Completed: req.body.Completed,
             Description: req.body.Description
         })
-        await task.save()
-            .then(response => {
-                res.json({
-                    status: 'success',
-                    data: {
-                        message: 'Task added to todo list in database'
-                    }
+        if (!(task.ToDo && task.Ongoing == true || task.ToDo && task.Completed == true || task.Ongoing && task.Completed == true)) {
+            await task.save()
+                .then(response => {
+                    res.status(200).json({
+                        status: 'Success',
+                        data: {
+                            message: 'Task added to todo list in database'
+                        }
+                    })
                 })
-            })
-            .catch((error) => {
-                res.json({
-                    status: 'failed',
-                    data: {
-                        message: 'Someting error occured while storing todo to database'
-                    }
+                .catch((error) => {
+                    res.status(400).json({
+                        status: 'Failed',
+                        data: {
+                            message: 'Someting error occured while storing todo to database'
+                        }
+                    })
+                    console.log(error)
                 })
-                console.log(error)
+        }
+        else {
+            res.status(400).json({
+                status: 'Failed',
+                response: {
+                    message: 'The states of tasks cannot be identical'
+                }
             })
+        }
     }
     catch (err) {
         res.status(400).json({
@@ -81,23 +91,33 @@ const updateTask = async (req, res, next) => {
             Ongoing: req.body.Ongoing,
             Completed: req.body.Completed,
         }
-        await Task.findByIdAndUpdate(taskID, { $set: updatedData })
-            .then(() => {
-                res.json({
-                    status: 'success',
-                    data: {
-                        message: 'State of Task Updated Successfully!'
-                    }
+        if (!(updatedData.ToDo && updatedData.Ongoing || updatedData.Ongoing && updatedData.Completed || updatedData.ToDo && updatedData.Completed)) {
+            await Task.findByIdAndUpdate(taskID, { $set: updatedData })
+                .then(() => {
+                    res.status(200).json({
+                        status: 'Success',
+                        data: {
+                            message: 'State of Task Updated Successfully!'
+                        }
+                    })
                 })
-            })
-            .catch(error => {
-                res.json({
-                    status: 'failed',
-                    data: {
-                        message: 'An error occured while updating state of task'
-                    }
+                .catch(error => {
+                    res.status(200).json({
+                        status: 'Failed',
+                        data: {
+                            message: 'An error occured while updating state of task'
+                        }
+                    })
                 })
+        }
+        else {
+            res.status(400).json({
+                status: 'Failed',
+                response: {
+                    message: "The updated states of tasks cannot be identical"
+                }
             })
+        }
     }
     catch (err) {
         res.status(400).json({
